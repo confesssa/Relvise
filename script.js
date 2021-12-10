d204490a1a25f947147ace11c6e63f706d0b8b83
volha <v.khviadchenia@gmail.com>
2021-12-10 23:47:48 +0100

    feat: add styles and functions for burger-menu

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-"use strict"
+function burgerMenu(){
+	burger = document.getElementsByClassName('burger__menu')[0]
+	headerMenu = document.getElementsByClassName('header__menu')[0]
+	burger.classList.toggle('burger__active');
+	headerMenu.classList.toggle('header__active');
+
+	secondLine = document.getElementsByClassName('burger__line')[0]
+	secondLine.classList.toggle('line__hide');
+
+	bodyLock = document.getElementById('body');
+	bodyLock.classList.toggle('body-lock');
+
+	console.log(bodyLock);
+}
+
+function burgerOnScroll() {
+	const header = document.querySelector('.header__container');
+	window.onscroll = () =>{
+		if(window.pageYOffset>80){
+			header.classList.add('header__container-active');
+		}
+		else{
+			header.classList.remove('header__container-active');
+		}
+	}
+};
+
+Window.onload = burgerOnScroll();
